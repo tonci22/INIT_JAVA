5e194cac9b73237d0e7eed23a07692c8799839c5
Ante Milina <ante.milina@hotmail.com>
2022-12-01 13:39:40 +0100

    fix no duplicate entries in Category

--- src/main/java/com/example/INIT_JAVA/services/Implementation/CategoryServiceImpl.java
+++ src/main/java/com/example/INIT_JAVA/services/Implementation/CategoryServiceImpl.java
@@ -31,6 +31,9 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public CategoryResponseDto add(CategoryRequestDto categoryRequestDto) {
 
+        if (categoryRepository.findByNameIn(List.of(categoryRequestDto.getName())).size() != 0)
+            throw new DuplicateEntityException("Category with this name already exists");
+
         Category category = categoryMapper.mapToDto(categoryRequestDto);
         categoryRepository.save(category);
 
